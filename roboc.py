e0f6b83e0816ed875fb86583f941c0a6dae60ef3
Thomas Grevedon <thomas.grevedon@gmail.com>
2018-10-24 13:21:45 +0200

    added a copy of the easy map to male some test and created more functionalities: check the move of robot in map, pritn the update map, option to quite the game or quite the fame if exit found

--- roboc.py
+++ roboc.py
@@ -14,15 +14,16 @@ number_of_map_avilables = game.found_map_avaialables()
 print("\n")
 valid_entry = False
 while valid_entry != True:
-    requested_map = str(input("Entrez un numéro de labyrinthe pour commencer à jouer : "))[:1]
+    requested_map = str(input("Select a number of map to start playing : "))[:1]
     try:
         requested_map = int(requested_map)
         assert requested_map < (number_of_map_avilables) and requested_map > 0
         map = "cartes/" + game.maps[requested_map - 1]
         map = Labyrinth(map)
+        robot = Robot(map.robot_position)
         valid_entry = True
     except ValueError:
-        print("Please enter a valid number")
+        print("\n" + "Please enter a valid number")
     except AssertionError:
         print("Sorry the number you have entered is not valid. You must select "
         "one of the offered number")
@@ -30,3 +31,48 @@ while valid_entry != True:
 print("\n")
 print(map)
 print("\n")
+print("Choose a letter among: \"n\", \"s\", \"o\", \"e\" foolowed by a number"
+"to requets a move. The number is optional, so you can only right a letter and"
+"the robot will try to move accoriding the" "desired direction by one step")
+"""We now let the player insert a move"""
+"""un first try pour etre sur que tout est isalpha"""
+"""--> while not q on ne quitte pas et idem si sortie trouvée"""
+"""--> comment enlever la porte et le x"""
+"""--> comment faire pour que ça reste dans le cadre du jeu"""
+"""--> comment réagir au mur (while empty ok sinon break)"""
+"""--> when click enter on move, starnge"""
+while exit != "q" and game.win == False:
+    valid_entry = False
+    allowed_entry = ["n", "s", "o", "e"]
+    while valid_entry != True:
+        move = input()
+        if len(move) == 1:
+            move += "1"
+        try:
+            issue = "wrong letter, you must choose bewteen \"n\", \"s\", \"o\", \"e\""
+            assert move[0].isalpha() and move[0].lower() in allowed_entry
+            issue = "Please insert a valid number after the direction"
+            assert move[1:].isdigit()
+            move_direction = move[0].lower()
+            number = move[1:]
+            #print("position before asking a move", map.robot_position)
+            robot_move = robot.move(move_direction, number)
+            #print("robot move", robot_move)
+            game.win = map.check_in_map(move_direction, robot_move, game)
+            #print(game.win)
+            robot.position = dict(map.robot_position)
+            #print("position of robot in the map", map.robot_position, "and in the robot itself", robot.position)
+            map.update_map()
+            print("\n")
+            print(map)
+            valid_entry = True
+        except AssertionError:
+            print(issue)
+            continue
+        except IndexError:
+            print("You msut enter a letter as you probably pressed enter without entering a letter")
+            continue
+        if game.win == False:
+            exit = str(input("Press q to quit and save or anything else (like enter) to continue: "))[:1].lower()
+
+game.quit()
--- roboc_classes.py
+++ roboc_classes.py
@@ -7,6 +7,8 @@ class Labyrinth:
         with open(name, "r") as file:
             self.lines = {}
             self.robot_position = {}
+            self.robot_initial_position = {}
+            self.point = False
             line = 1
             for object in file.read():
                 if object != "\n":
@@ -28,21 +30,117 @@ class Labyrinth:
                 map_to_print += object
         return map_to_print
 
+    def check_in_map(self, move_direction, position, game):
+        self.robot_initial_position = dict(self.robot_position)
+        #print("before any check in map", self.robot_position)
+        position_line = position["line"]
+        position_index = position["index"]
+        if position_line not in self.lines:
+            print("---->You are out of the map, try again")
+            return game.win
+        if position_index > (len(self.lines[self.robot_position["line"]]) - 1) or \
+        position_index < 0:
+            print("---->You are out of the map, try again")
+            return game.win
+        if move_direction == "n":
+            while self.robot_position["line"] != position_line:
+                object = self.lines[self.robot_position["line"] - 1][self.robot_position["index"]]
+                if object == "O":
+                    break
+                elif object == "U":
+                    self.robot_position["line"] -=1
+                    print("----->You have won!! Well done! You are out of the labyrinthe<--------")
+                    game.win = True
+                else:
+                    self.robot_position["line"] -=1
+            return game.win
+        if move_direction == "o":
+            while self.robot_position["index"] != position_index:
+                object = self.lines[self.robot_position["line"]][self.robot_position["index"] - 1]
+                if object == "O":
+                    break
+                elif object == "U":
+                    self.robot_position["index"] -=1
+                    print("----->You have won!! Well done! You are out of the labyrinthe<--------")
+                    game.win = True
+                else:
+                    self.robot_position["index"] -=1
+            return game.win
+        if move_direction == "s":
+            while self.robot_position["line"] != position_line:
+                object = self.lines[self.robot_position["line"] + 1][self.robot_position["index"]]
+                if object == "O":
+                    break
+                elif object == "U":
+                    self.robot_position["line"] +=1
+                    print("----->You have won!! Well done! You are out of the labyrinthe<--------")
+                    game.win = True
+                else:
+                    self.robot_position["line"] +=1
+            return game.win
+        if move_direction == "e":
+            while self.robot_position["index"] != position_index:
+                object = self.lines[self.robot_position["line"]][self.robot_position["index"] + 1]
+                if object == "O":
+                    break
+                elif object == "U":
+                    self.robot_position["index"] +=1
+                    print("----->You have won!! Well done! You are out of the labyrinthe<--------")
+                    game.win = True
+                else:
+                    self.robot_position["index"] +=1
+            return game.win
+
+    def update_map(self):
+        line_to_empty = self.robot_initial_position["line"]
+        index_to_empty = self.robot_initial_position["index"]
+        line_to_fill = self.robot_position["line"]
+        index_to_fill = self.robot_position["index"]
+        if self.point:
+            self.lines[line_to_empty][index_to_empty] = "."
+            self.point = False
+        else:
+            self.lines[line_to_empty][index_to_empty] = " "
+        if self.lines[line_to_fill][index_to_fill] == ".":
+            self.point = True
+            self.lines[line_to_fill][index_to_fill] = "X"
+        else:
+            self.lines[line_to_fill][index_to_fill] = "X"
+
+
 class Robot:
 
     def __init__(self, position):
         self.position = dict(position)
 
+    def move(self, move_direction, number):
+        if move_direction == "n":
+            self.position["line"] -= int(number)
+            return self.position
+        if move_direction == "s":
+            self.position["line"] += int(number)
+            return self.position
+        if move_direction == "o":
+            self.position["index"] -= int(number)
+            return self.position
+        if move_direction == "e":
+            self.position["index"] += int(number)
+            return self.position
+
 class Game:
 
     def __init__(self, maps = []):
         self.maps = maps
+        self.win = False
 
     def found_map_avaialables(self):
-        print("Labyrinthes existants :")
+        print("Existing labyrinths :")
         i = 1
         for file in os.listdir("cartes"):
             print("  ", i, " - ", file[:-4])
             i += 1
             self.maps.append(file)
         return i
+
+    def quit(self):
+        print("We are going to leave and save")
