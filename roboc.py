9f6c357d71a87daef3ca6f21d004c39a2b0f10d3
Thomas Grevedon <thomas.grevedon@gmail.com>
2018-10-25 13:28:46 +0200

    comented code and uploaded it

--- roboc.py
+++ roboc.py
@@ -2,30 +2,46 @@
 from roboc_classes import *
 
 """
-carte = Labyrinth("cartes/facile.txt")
-print(carte)
-print(carte.robot_position)
-print(carte)
+This a game called Roboc where the player is driving a robot trough a
+Labyrinth and needs to try to find the exit.
+The player is first asked to select an avalaible map and to decide to continue
+playing a previous game if this map xas previously used without finishing it.
+After the player decides, the game starts and the player must enter a letter
+with a number to move the robot:
+    - "n": moves the robot one step up
+    - "s11": moves the robot 11 setps down
+    - "o2": moves the robot 2 steps left
+    - "e": moves the robot 1 step right
+If the robot encounters a wall(O), it will move until the wall and stop.
+If the number of move is above the length of the map, the robot will not move at all.
+If the robot finds a door(.) it can go trough it.
+If the robot finds the exit(U), the player has won and the game is finished
+After each moves, the player is asked to quit and saved and can do it by
+pressing "q". Any other entry continue the game. After each move, an automatic
+save is done (in case the program is stoped for any reason the player would
+be allwoed to start at the previous place where he was).
 """
-""" we check how manu availables maps we have, what are they and we ask player
- to select one"""
+
+"""first we get the map and when the player decide which map he wants to use,
+        we start the game"""
 game = Game()
-number_of_map_avilables = game.found_map_avaialables()
+number_of_map_avilables = game.found_map_avaialables() #we check how many maps are availables
 print("\n")
 valid_entry = False
 while valid_entry != True:
-    requested_map = str(input("Select a number of map to start playing : "))[:1]
+    requested_map = str(input("Select a number of map to start playing : "))[:1] #this number - 1 will be the index for getting the map in the attribute self.maps
     try:
         requested_map = int(requested_map)
         assert requested_map < (number_of_map_avilables) and requested_map > 0
-        map_name = "cartes/" + game.maps[requested_map - 1]
-        map, on_going_party = game.unpack_map(map_name)
-        if on_going_party:
+        map_name = "cartes/" + game.maps[requested_map - 1] #we can then place this name to request a Labyrinth
+        map, on_going_game = game.unpack_map(map_name) #we check if there is an existing game for this map
+        if on_going_game:
             continue_play = str(input("Type \"y\" if you want to continue playing"
             " or press anything else if you want to start a new party: "))[:1].lower()
             if continue_play != "y":
-                map = Labyrinth(map_name)
-        else:
+                game.delete_existing_game(map_name, map) #we delete the existing game for the map
+                map = Labyrinth(map_name) #we start with a renewed map
+        else: #no existing game, we start with a new map
             map = Labyrinth(map_name)
         robot = Robot(map.robot_position)
         valid_entry = True
@@ -35,26 +51,30 @@ while valid_entry != True:
         print("Sorry the number you have entered is not valid. You must select "
         "one of the offered number")
 
+
 print("\n")
 print(map)
 print("\n")
-print("Choose a letter among: \"n\", \"s\", \"o\", \"e\" foolowed by a number"
-"to requets a move. The number is optional, so you can only right a letter and"
+print("Choose a letter among: \"n\", \"s\", \"o\", \"e\" foolowed by a number "
+"to requets a move. The number is optional, so you can only right a letter and "
 "the robot will try to move accoriding the" "desired direction by one step")
-"""We now let the player insert a move"""
-"""un first try pour etre sur que tout est isalpha"""
-"""--> while not q on ne quitte pas et idem si sortie trouvée"""
-"""--> comment enlever la porte et le x"""
-"""--> comment faire pour que ça reste dans le cadre du jeu"""
-"""--> comment réagir au mur (while empty ok sinon break)"""
-"""--> when click enter on move, starnge"""
+
+"""We now
+        -let the player ask for a move
+        -check if it is possible
+        -update the robot position after the check
+        -update and print the map
+        -save automatically
+        -ask to quit and save or continue playing
+        """
+
 while exit != "q" and game.win == False:
     valid_entry = False
     allowed_entry = ["n", "s", "o", "e"]
     while valid_entry != True:
         move = input()
         if len(move) == 1:
-            move += "1"
+            move += "1" #in case only a letter has been entered, we add one so we will request one step on the move
         try:
             issue = "wrong letter, you must choose bewteen \"n\", \"s\", \"o\", \"e\""
             assert move[0].isalpha() and move[0].lower() in allowed_entry
@@ -62,14 +82,11 @@ while exit != "q" and game.win == False:
             assert move[1:].isdigit()
             move_direction = move[0].lower()
             number = move[1:]
-            #print("position before asking a move", map.robot_position)
-            robot_move = robot.move(move_direction, number)
-            #print("robot move", robot_move)
-            game.win = map.check_in_map(move_direction, robot_move, game)
-            #print(game.win)
-            robot.position = dict(map.robot_position)
-            #print("position of robot in the map", map.robot_position, "and in the robot itself", robot.position)
+            robot_move = robot.move(move_direction, number) #we ask the robot to move by updating its position
+            game.win = map.check_in_map(move_direction, robot_move, game) #we check in map if the move is possible and if the player has won
+            robot.position = dict(map.robot_position) #we update the robot position with the position of robot in map after updating it.
             map.update_map()
+            game.automatic_save(map_name, map)
             print("\n")
             print(map)
             valid_entry = True
@@ -82,4 +99,6 @@ while exit != "q" and game.win == False:
         if game.win == False:
             exit = str(input("Press q to quit and save or anything else (like enter) to continue: "))[:1].lower()
 
+"""last part is to quit the game and update the file with all existing games
+according if the player has won or not"""
 game.quit(map_name, game.win, map)
--- roboc_classes.py
+++ roboc_classes.py
@@ -2,20 +2,33 @@ import copy
 import os
 import pickle
 
-class Labyrinth:
+"""This contains all classes with their fucntions that will be used by the
+    program roboc.py"""
 
+class Labyrinth:
+    """class Labyrinth that will be used to initialize the map and
+        print it"""
     def __init__(self, name):
+        """--> The initialize function will receive a map name that is
+        chosen by the player and will read the objects  in the
+        designed map in the folder: cartes.
+          --> the construction of the map follows this way: every object is
+          saved in a list of objects that are on the same line. When the object
+          is "\n", a new line is created with a new list. The whole set of
+          {lines: [objects]} is saved in a dictionary allowing the program to
+          analyse any object at a designed line.
+           """
         with open(name, "r") as file:
             self.lines = {}
-            self.robot_position = {}
-            self.robot_initial_position = {}
-            self.point = False
+            self.robot_position = {} #the real position of the robot on the map with {line: index}
+            self.robot_initial_position = {} #the position of the robot before a move with {line: index}
+            self.point = False #will be used to check if the player is on a door
             line = 1
             for object in file.read():
                 if object != "\n":
                     if line not in self.lines:
                         self.lines[line] = []
-                    self.lines[line].append(object)
+                    self.lines[line].append(object) #we append each object on the list of the line
                     if object == "X":
                         self.robot_position["line"] = line
                         self.robot_position["index"] = self.lines[line].index(object)
@@ -24,23 +37,36 @@ class Labyrinth:
 
     def __str__(self):
         map_to_print = ""
-        dict_copy_lines = copy.deepcopy(self.lines)
+        dict_copy_lines = copy.deepcopy(self.lines) #we make a copy to avoid modifying the object attribute
         for position in dict_copy_lines.keys():
-            dict_copy_lines[position].append('\n')
+            dict_copy_lines[position].append('\n') #we add again the space at the end of each line to create a new line
             for object in dict_copy_lines[position]:
                 map_to_print += object
         return map_to_print
 
     def __getstate__(self):
+        """a special method to save the current dictionary of lines of the map.
+         This will allow the game to come back to its original state after the
+         player quit without finishing the game"""
         dict_attr = dict(self.__dict__)
         dict_attr["lines"] = self.lines
         return dict_attr
 
     def check_in_map(self, move_direction, position, game):
+        """This represents a big part of the program.
+        For each move required by the player, we first analyse if the number
+        of moves required will not bring the player out of the map.
+        If it is fine, we anlyse if the move is "n" or "s" for a move on
+        lines or "e", "o" for a move on indexes.
+        We analyse then each object on the way of the move to see if it is a wall (O),
+        a door(.) or the exit(U).
+        For each move we update the real position of the robot on the map.
+        The function should return if the player has won or not """
+        #we first make a copy of the real position of the robot before sartting the move and update the robot position
+        #this will be helpfull when updating the map as we can act on the inital position and on the new position
         self.robot_initial_position = dict(self.robot_position)
-        #print("before any check in map", self.robot_position)
-        position_line = position["line"]
-        position_index = position["index"]
+        position_line = position["line"] #This is the line where we want the robot to move
+        position_index = position["index"] #This is the index where we want the robot to move
         if position_line not in self.lines:
             print("---->You are out of the map, try again")
             return game.win
@@ -55,7 +81,7 @@ class Labyrinth:
                     break
                 elif object == "U":
                     self.robot_position["line"] -=1
-                    print("----->You have won!! Well done! You are out of the labyrinthe<--------")
+                    print("----->You have won!! Well done! You are out of the labyrinth<--------")
                     game.win = True
                 else:
                     self.robot_position["line"] -=1
@@ -67,7 +93,7 @@ class Labyrinth:
                     break
                 elif object == "U":
                     self.robot_position["index"] -=1
-                    print("----->You have won!! Well done! You are out of the labyrinthe<--------")
+                    print("----->You have won!! Well done! You are out of the labyrinth<--------")
                     game.win = True
                 else:
                     self.robot_position["index"] -=1
@@ -91,13 +117,17 @@ class Labyrinth:
                     break
                 elif object == "U":
                     self.robot_position["index"] +=1
-                    print("----->You have won!! Well done! You are out of the labyrinthe<--------")
+                    print("----->You have won!! Well done! You are out of the labyrinth<--------")
                     game.win = True
                 else:
                     self.robot_position["index"] +=1
             return game.win
 
     def update_map(self):
+        """This will help to update the dictionary lines accoriding the new position
+        of the robot after a move.
+        If the move ends on a door (.), the attribute self.point will be changed
+        to True so we can put the door back on the next move """
         line_to_empty = self.robot_initial_position["line"]
         index_to_empty = self.robot_initial_position["index"]
         line_to_fill = self.robot_position["line"]
@@ -115,11 +145,14 @@ class Labyrinth:
 
 
 class Robot:
+    """We define the obejct robot with its position gotten from its position on the map."""
 
     def __init__(self, position):
         self.position = dict(position)
 
     def move(self, move_direction, number):
+        """When a player request a move, the position of the robot will be updated and will be
+        used in the map to check if the move is possible. """
         if move_direction == "n":
             self.position["line"] -= int(number)
             return self.position
@@ -134,12 +167,20 @@ class Robot:
             return self.position
 
 class Game:
-
+    """This class will be used to show all map availables for the game.
+    It has the function to check if a game was on going to take back the map to its previous state.
+    It also has the possibily to save automatically each move on the game as well as to delete
+    an existing map if the play wants to start a new game"""
     def __init__(self, maps = []):
         self.maps = maps
-        self.win = False
+        self.win = False #will be used to leave the game if True
 
     def found_map_avaialables(self):
+        """This will read the titles of availables maps in the folder "cartes".
+        Each map name will be added on the list of maps in the attribute. Then
+        when the player will choose, the map name will be used to initialize the Labyrinth.
+        We have a counter that we update on each read so we can add this number in the message
+        when we ask the player to choose a map"""
         print("Existing labyrinths :")
         i = 1
         for file in os.listdir("cartes"):
@@ -149,39 +190,69 @@ class Game:
         return i
 
     def unpack_map(self, map_name):
+        """This function will be used to check if a previous game was started
+        but not finished. It is checked with the map_name in a dictionary of the saved games.
+        If it is the case, we will return the map and ask the player if if wants
+        to continue playing or start a new party."""
         try:
             with open("map_history", "rb") as file:
                 mon_unpickler = pickle.Unpickler(file)
                 dictionary = mon_unpickler.load()
-                if map_name in dictionary:
+                if map_name in dictionary: #there is a previous game not finished
                     map = dictionary[map_name]
-                    on_going_party = True
-                    return map, on_going_party
-                else:
+                    on_going_game = True
+                    return map, on_going_game #we return the map in it previous state
+                else: #no previous game was found
                     map = ""
-                    on_going_party = False
-                    return map, on_going_party
-        except FileNotFoundError:
+                    on_going_game = False
+                    return map, on_going_game
+        except FileNotFoundError: #the file is not creatd so we create it
             with open("map_history", "wb") as file:
                 mon_pickle = pickle.Pickler(file)
                 dictionary = {}
                 mon_pickle.dump(dictionary)
                 map = ""
-                on_going_party = False
-                return map, on_going_party
+                on_going_game = False
+                return map, on_going_game
 
-    def quit(self, map_name, win, map):
-        if win == False:
+    def delete_existing_game(self, map_name, map):
+        """if there was a previous game on going but the player wants to start
+        a new game, we delete the previous state of the map"""
+        with open("map_history", "rb") as file:
+            mon_unpickler = pickle.Unpickler(file)
+            dictionary = mon_unpickler.load()
             with open("map_history", "wb") as file:
                 my_pickle = pickle.Pickler(file)
-                dictionary = {}
+                del dictionary[map_name]
+                my_pickle.dump(dictionary)
+
+    def automatic_save(self, map_name, map):
+        """After each move, this function will be called to save the map states"""
+        with open("map_history", "rb") as file:
+            mon_unpickler = pickle.Unpickler(file)
+            dictionary = mon_unpickler.load()
+            with open("map_history", "wb") as file:
+                my_pickle = pickle.Pickler(file)
                 dictionary[map_name] = map
                 my_pickle.dump(dictionary)
-                print("We are going to leave and save")
-        else:
-            with open("map_history", "wb") as file:
-                my_pickle = pickle.Pickler(file)
-                dictionary = {}
-                if map_name in dictionary:
-                    del dictionary[map_name]
-                my_pickle.dump(dictionary)
+
+    def quit(self, map_name, win, map):
+        """if the player decides to quit and did not win, we do a last save.
+        if the player won, the map states is deleted so the next time, the game
+        will start immediately with a renewed map"""
+        with open("map_history", "rb") as file:
+            mon_unpickler = pickle.Unpickler(file)
+            dictionary = mon_unpickler.load()
+            if win == False:
+                with open("map_history", "wb") as file:
+                    my_pickle = pickle.Pickler(file)
+                    dictionary[map_name] = map
+                    my_pickle.dump(dictionary)
+                    print("Thank you for having played. Your game is saved. "
+                    "See you soon!")
+            else:
+                with open("map_history", "wb") as file:
+                    my_pickle = pickle.Pickler(file)
+                    if map_name in dictionary:
+                        del dictionary[map_name]
+                    my_pickle.dump(dictionary)
